1de4bf3f760d8a71efe9c4ec3647deaa3c389689
DechenTshomo <dechentshomo2218@gmail.com>
2023-07-19 14:36:21 +0600

    Frontend update

--- app.js
+++ app.js
@@ -1,22 +1,81 @@
-// /** @format */
+/** @format */
+
+// imports
 
 const express = require("express");
 const app = express();
-
-const viewRouter = require('./routes/viewRoutes')
 const path = require("path");
+const port = 3000;
 
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
 
-app.use('/', viewRouter)
-app.use(express.static(path.join(__dirname, 'views')))
-module.exports = app
+// set views
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
+// app.set('views', path.join(__dirname, "views"));
+//
+app.get("", (req, res) => {
+  res.render("index");
+});
 
-app.listen(3000, function () {
-  console.log("App running on port 3000 ..");
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 
-app.use(express.static(path.join(__dirname, 'view')))
-module.exports = app
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/forgotpass", (req, res) => {
+  res.render("forgotpass");
+});
+
+app.get("/resetpass", (req, res) => {
+  res.render("resetpass");
+});
+
+app.get("/newrelease", (req, res) => {
+  res.render("newrelease");
+});
+
+app.get("/upcoming", (req, res) => {
+  res.render("upcoming");
+});
+
+app.get("/booking", (req, res) => {
+  res.render("booking");
+});
+
+app.get("/details", (req, res) => {
+  res.render("details");
+});
+
+app.get("/summary", (req, res) => {
+  res.render("summary");
+});
+
+app.get("/bookinghistory", (req, res) => {
+  res.render("bookinghistory");
+});
+
+app.get("/rating", (req, res) => {
+  res.render("rating");
+});
+app.get("/moviedetails", (req, res) => {
+  res.render("moviedetails");
+});
+
+
+// listen on port
+app.listen(port, () => console.info(`Listening on port ${port}`));
