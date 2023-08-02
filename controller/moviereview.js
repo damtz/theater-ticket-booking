b1d33ed3c676930ae8c0e5e44f4c1e01d62a2a95
Bikram Chuwan <57659971+bikram1234@users.noreply.github.com>
2023-08-02 11:48:36 +0600

    Update moviereview.js

--- controller/moviereview.js
+++ controller/moviereview.js
@@ -7,6 +7,7 @@ const isLoggedin = function (req, res, next) {
   if (req.isAuthenticated()) {
     next();
   } else {
+    req.flash('error','Yor are not Authorized..')
     res.redirect('/login');
   }
 };
@@ -16,7 +17,7 @@ function ensureUser(req, res, next) {
     // If the user is logged in and has the role "user," proceed to the next middleware or route handler
     return next();
   } else {
-    // If the user is not logged in or doesn't have the role "user," redirect to the desired route (e.g., home page)
+    req.flash('error','Yor are not Authorized..')
     res.redirect('/login');
   }
 }
